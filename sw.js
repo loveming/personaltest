9f5ad4c4c4cd5a4d6e59d74797a16b7d39cf3293
loveming <517965135@qq.com>
2018-08-17 16:04:03 +0800

    test

--- sw.js
+++ sw.js
@@ -20,3 +20,17 @@ self.addEventListener('fetch', function (event) { // 监控本地fetch事件
     })
   )
 })
+
+self.addEventListener('push', function (event) {
+  // 检查服务端是否发来了任何有效载荷数据
+   var payload = event.data ? JSON.parse(event.data.text()) : 'no payload';
+   var title = 'Progressive Times';
+   event.waitUntil(
+     // 使用提供的信息来显示 Web 推送通知
+     self.registration.showNotification(title, {                           
+       body: payload.msg,
+       url: payload.url,
+       icon: payload.icon
+     })
+   );
+ });
